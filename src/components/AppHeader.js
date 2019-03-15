c109ab115682b67682281aa380c12a2a90e84e4c
rndev80 <rndev80@gmail.com>
2019-03-16 00:28:29 +0100

    Displaying session data and signing out

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,14 +1,54 @@
 import React from "react"
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import $ from 'jquery';
+
 class AppHeader extends React.Component {
+
+  componentDidMount () {
+		$.ajax({
+			type: 'GET',
+			url: 'http://localhost:3001/auth/validate_token',
+      dataType: "JSON",
+      headers: JSON.parse(sessionStorage.getItem('user'))
+		})
+		.fail((data) => {
+			this.props.history.push('/login');
+		})
+  }
+
+  handleSignOut = (e) => {
+		e.preventDefault();
+    
+    $.ajax({
+			type: 'DELETE',
+			url: 'http://localhost:3001/auth/sign_out',
+			data: JSON.parse(sessionStorage.user),
+		})
+		.done(() => {
+			sessionStorage.removeItem('user');
+			this.props.history.push('/login');
+    });
+
+  }
+
   render () {
-    return (
-      <React.Fragment>
-        <Link to="/">
+    if(sessionStorage.getItem('user')) {
+      return (
+        <React.Fragment>
+          <p>
+            {JSON.parse(sessionStorage.getItem('user')).uid}
+            <a href="/" onClick={this.handleSignOut} >Sign out</a>
+          </p>
+          <Link to="/">
             <h1>CalReact</h1>
-        </Link>
-      </React.Fragment>
-    );
+          </Link>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <Redirect to='/login' />
+      );
+    }
   }
 }
 
